028cd81adaa2ca25771c06c455a73ccfb9ae543f
Omanshu Aman <omanshuaman@gmail.com>
2022-11-06 17:00:24 +0530

    auth

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,25 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const { chats } = require("./data/data");
 const connectDB = require("./config/db");
-const colors = require("colors");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
-
 connectDB();
 const app = express();
+
+app.use(express.json()); // to accept json data
 
 app.get("/", (req, res) => {
   res.send("API Running!");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
 
-app.get("/api/chat/:id", (req, res) => {
-  console.log(req.params.id);
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, console.log(`Server running on PORT ${PORT}...`.yellow.bold));
+app.listen(PORT, console.log(`Server running on PORT ${PORT}...`));
